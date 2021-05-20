67ceceb6357bcaef053fd3c1fe477c87423a0b82
iseluin <kutay.kagan.ozenn@gmail.com>
2021-05-20 09:01:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+class Polygon {
+    constructor([...sides]) {
+        this.sides = sides;
+    }
+
+    get countSides () {
+        return this.sides.length;
+    }
+
+    get perimeter () {
+
+        // let perimeter = 0;
+
+        // for (let i = 0; i < this.sides.length; i++) {
+        //     perimeter += this.sides[i];     
+        // }
+
+        // return perimeter;
+
+        let perimeter = this.sides.reduce((acc, curr) => {return acc + curr}, 0);
+
+        return perimeter;
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid () {
+        let validifier = this.sides.every(side => side > 0)
+        return validifier;
+    }
+}
